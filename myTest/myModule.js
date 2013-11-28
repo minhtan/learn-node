5c55fe197010af5cf438cdc777f0ccfccdc151a6
tan <tannm@smartosc.com>
2013-11-28 17:06:10 +0700

    :D

---
+++ myTest/myModule.js
@@ -0,0 +1,3 @@
+http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
+http://yuiblog.com/blog/2007/06/12/module-pattern/
+http://stackoverflow.com/questions/2713431/javascript-module-pattern-what-about-using-return-this

c202cdd71286a7db0ca7e9c917535a49f9d11526
m.tan <m.tan2007@gmail.com>
2013-11-28 21:52:36 +0700

    :D

--- myTest/hello.js
+++ myTest/hello.js
@@ -1,5 +1,11 @@
-var sum = 0;
-for(var i=2; i<process.argv.length; i++){
-	sum = sum + Number(process.argv[i]);
-}
-console.log(sum);
+// 1. hello world
+// console.log("HELLO WORLD");
+
+// 2. baby step
+// var sum = 0;
+// for(var i=2; i<process.argv.length; i++){
+// 	sum = sum + Number(process.argv[i]);
+// }
+// console.log(sum);
+
+// 3. my first IO

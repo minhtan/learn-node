284c52366f5b34af9b1bee7b4f24ca40fb5f2eee
tan <tannm@smartosc.com>
2013-11-29 11:12:27 +0700

    :D

--- myTest/hello.js
+++ myTest/hello.js
@@ -1,6 +1,7 @@
 // 1. hello world
 // console.log("HELLO WORLD");
 
+
 // 2. baby step
 // var sum = 0;
 // for(var i=2; i<process.argv.length; i++){
@@ -8,11 +9,13 @@
 // }
 // console.log(sum);
 
+
 // 3. my first IO
 // var fs = require('fs');
 // var content = fs.readFileSync(process.argv[2]);
 // var lines = content.toString().split('\n');
 // console.log(lines.length-1);
+
 
 // 4. my first async IO
 // var fs = require('fs');
@@ -22,32 +25,36 @@
 //  console.log(lines.length-1);
 // });
 
-// 5. filtered LS
-// var fs = require('fs');
-// var myPath = process.argv[2];
-// var ext = '.' + process.argv[3];
 
-// // 5a.
+// 5. filtered LS
+// var fs = require('fs')
+// var myPath = process.argv[2]
+// var ext = '\\.' + process.argv[3] + '$'
+// var ext2 = '.' + process.argv[3]
+
+// 5a.
 // fs.readdir(myPath, function (err, list){
 // 	if (err) throw err;
 // 	var patt = new RegExp(ext);
-// 	for(var i=0; i<list.length; i++){
-// 		if(list[i].match(patt)){
-// 			console.log(list[i]);
-// 		}
-// 	}
+// 	list.forEach(function (file) {
+// 	    if (patt.test(file))
+// 	      console.log(file);
+//   	});
 // });
 
 // 5b.
 // fs.readdir(myPath, function (err, list){
-// 	if (err) throw err;
-// 	var resultList = list.filter(function(f){
-// 		return f.substr(-(ext.length)) == ext;
-// 	});
-// 	for(var i=0; i<resultList.length; i++){
-// 		console.log(resultList[i]);
-// 	}
-// });
+// 	if (err) throw err
+// 	var resultList = list.filter(function (file) {
+// 		return file.substr(-(ext2.length)) == ext2
+// 	})
+// 	resultList.forEach(function (file) {
+// 		console.log(file)
+// 	})
+// })
+
+// WOW semicolons arent even necessary!
+
 
 // 6. make it moduler
-var fs = require('fs');
+var fs = require('fs')
